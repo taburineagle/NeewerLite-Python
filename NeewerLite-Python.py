ce8fc5d27f4784cf4b865011647da0f6914572f5
Zach Glenwright <zachglenwright@comcast.net>
2022-03-17 07:18:46 -0400

    3-17-22 A - Text encoding tweaking
    Set text encoding of all file writes and reads (except for the lockfile) to ensure they're UTF-8 encoded, and tidied up a few lines (not much operationally different, just nicer code).  Also added in META tag explicitly telling the HTML renderer that the content being delivered is UTF-8 formatted, to work more nicely with #35 and accented characters in light names.

--- NeewerLite-Python.py
+++ NeewerLite-Python.py
@@ -843,7 +843,7 @@ try: # try to load the GUI
                 finalPrefs.append("enableTabsOnLaunch=1")
                
             if len(finalPrefs) > 0: # if we actually have preferences to save...
-                with open(globalPrefsFile, "w") as prefsFileToWrite:
+                with open(globalPrefsFile, mode="w", encoding="utf-8") as prefsFileToWrite:
                     prefsFileToWrite.write(("\n").join(finalPrefs)) # then write them to the prefs file
 
                 # PRINT THIS INFORMATION WHETHER DEBUG OUTPUT IS TURNED ON OR NOT
@@ -1032,7 +1032,7 @@ try: # try to load the GUI
                     exportString += "|" + "120,135,2,100,56,157" # then just give the default (CCT, 5600K, 100%) params
 
             # WRITE THE PREFERENCES FILE
-            with open(exportFileName, "w") as prefsFileToWrite:
+            with open(exportFileName, mode="w", encoding="utf-8") as prefsFileToWrite:
                 prefsFileToWrite.write(exportString)
 
             if customName != "":
@@ -1305,7 +1305,7 @@ try: # try to load the GUI
                     createLightPrefsFolder() # create the light_prefs folder if it doesn't exist
 
                     # WRITE THE PREFERENCES FILE
-                    with open(customLightPresetsFile, "w") as prefsFileToWrite:
+                    with open(customLightPresetsFile, mode="w", encoding="utf-8") as prefsFileToWrite:
                         prefsFileToWrite.write("\n".join(customPresetsToWrite))
 
                     printDebugString("Exported custom presets to " + customLightPresetsFile)
@@ -1736,9 +1736,8 @@ def loadCustomPresets():
     global customLightPresets
 
     # READ THE PREFERENCES FILE INTO A LIST
-    fileToOpen = open(customLightPresetsFile)
-    customPresets = fileToOpen.read().split("\n")
-    fileToOpen.close()
+    with open(customLightPresetsFile, mode="r", encoding="utf-8") as fileToOpen:
+        customPresets = fileToOpen.read().split("\n")
 
     acceptable_arguments = ["customPreset0", "customPreset1", "customPreset2", "customPreset3", \
                             "customPreset4", "customPreset5", "customPreset6", "customPreset7"]
@@ -1971,9 +1970,8 @@ def getCustomLightPrefs(MACAddress, lightName = ""):
         printDebugString("A custom preferences file was found for " + MACAddress + "!")
 
         # READ THE PREFERENCES FILE INTO A LIST
-        fileToOpen = open(customPrefsPath)
-        customPrefs = fileToOpen.read().split("|")
-        fileToOpen.close()
+        with open(customPrefsPath, mode="r", encoding="utf-8") as fileToOpen:
+            customPrefs = fileToOpen.read().split("|")
 
         # CHANGE STRING "Booleans" INTO ACTUAL BOOLEANS
         for b in range(1,3):
@@ -2789,6 +2787,7 @@ def writeHTMLSections(self, theSection, errorMsg = ""):
     elif theSection == "htmlheaders":
         self.wfile.write(bytes("<!DOCTYPE html>\n", "utf-8"))
         self.wfile.write(bytes("<HTML>\n<HEAD>\n", "utf-8"))
+        self.wfile.write(bytes("<META HTTP-EQUIV='Content-Type' CONTENT='text/html;charset=UTF-8'>\n", "utf-8"))
         self.wfile.write(bytes("<TITLE>NeewerLite-Python 0.11 HTTP Server by Zach Glenwright</TITLE>\n</HEAD>\n", "utf-8"))
         self.wfile.write(bytes("<BODY>\n", "utf-8"))
     elif theSection == "errorHelp":
@@ -2864,9 +2863,8 @@ def loadPrefsFile(globalPrefsFile = ""):
     if globalPrefsFile != "":
         printDebugString("Loading global preferences from file...")
 
-        fileToOpen = open(globalPrefsFile)
-        mainPrefs = fileToOpen.read().splitlines()
-        fileToOpen.close()
+        with open(globalPrefsFile, mode="r", encoding="utf-8") as fileToOpen:
+            mainPrefs = fileToOpen.read().splitlines()
 
         acceptable_arguments = ["findLightsOnStartup", "autoConnectToLights", "printDebug", "maxNumOfAttempts", "rememberLightsOnExit", "acceptableIPs", \
             "SC_turnOffButton", "SC_turnOnButton", "SC_scanCommandButton", "SC_tryConnectButton", "SC_Tab_CCT", "SC_Tab_HSI", "SC_Tab_SCENE", "SC_Tab_PREFS", \
