5224d2d01fb7f044a56ed3382905f8b5dd168572
Zach Glenwright <zachglenwright@comcast.net>
2025-02-04 15:05:30 -0500

    2-4-25 - Changed light type for RGB168
    To troubleshoot #95, I switched the RGB168 setting back to Infinity-esque mode.

--- NeewerLite-Python.py
+++ NeewerLite-Python.py
@@ -3471,7 +3471,7 @@ def getLightSpecs(lightName, returnParam = "all") -> list:
         ["RGB1000", 2500, 10000, False, 1],
         ["RGB1200", 2500, 10000, False, 1],
         ["RGB140", 2500, 10000, False, 1],
-        ["RGB168", 2500, 8500, False, 1],
+        ["RGB168", 2500, 8500, False, 2],
         ["RGB176", 3200, 5600, False, 0],
         ["RGB176 A1", 2500, 10000, False, 0],
         ["RGB18", 3200, 5600, False, 0],
